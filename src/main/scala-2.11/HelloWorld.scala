0d39e641a5da29b159dfad14987079de4631b420
Ildus <knifenn@yandex.ru>
2016-01-08 00:16:13 +0300

    HW#3

--- src/main/scala-2.11/HelloWorld.scala
+++ src/main/scala-2.11/HelloWorld.scala
@@ -1,32 +1,78 @@
 abstract class Person(name : String, age : Integer = 0){
-    def getInfo(value : String) = {
-        println(value)
-    }
+    def getInfo: String
 }
 
 trait ScienceWork{
-    def setNewAcademicDegree(degree: String) : String = degree + "123"
+    def setNewAcademicDegree(degree: String) : String = degree
 }
 
-class Teacher(academicDegree: String) extends Person(academicDegree) with ScienceWork{
-    def teach(condition : Condition) : String = {
-        println(condition)
-        return condition.toString
+class Teacher(name: String, age: Integer, var academicDegree: String) extends Person(academicDegree) with ScienceWork{
+    override def getInfo : String = {
+        "Teacher: " + name + ", age: " + age + ", academic Degree: " + academicDegree
     }
+    def teach(condition : Condition) : String = {
+        condition.toString
+    }
+
 }
 
 case class Condition(condition : String)
 
-class Student(course : Integer, isRecovered : Boolean = false){
+class Student(name: String, age: Integer, val course: Integer, val isRecovered: Boolean = false) extends Person(name, age){
     def study(mood : Boolean) : String = {
         return mood.toString
     }
+    override def getInfo: String = {
+        "Student: " + name + ", age: " + age + ", is recovered: " + isRecovered
+    }
+
 }
 
 object Student{
-    def apply(name : String, age : Integer) : Student = {
-        val std = new Student(4,true);
-        return std;
+    def apply(name: String, age : Integer) = {
+        new Student(name, age, 1)
     }
+
+    def apply(name: String, age : Integer, course: Integer) = {
+        new Student(name, age, course)
+    }
+
+    def apply(name: String, age : Integer, course: Integer, isRecovered: Boolean) = {
+        new Student(name, age, course, isRecovered)
+    }
+    implicit class Converter(student: String){
+        def toStudent: Student = {
+            val split = student.split(":")
+            split.size match {
+                case 2 => Student(split(0),split(1).toInt)
+                case 3 => Student(split(0),split(1).toInt,split(2).toInt)
+                case 4 => Student(split(0),split(1).toInt,split(2).toInt, split(3).toBoolean)
+                case _ => throw new Exception("Invalid argument")
+            }
+        }
+    }
+
+    def apply(source: String) = {
+        source.toStudent
+    }
+
+
 }
+
+object MainObj extends App{
+    val student = Student("Kayumov Ildus:20")
+    println(student.getInfo)
+
+    def roulette(chamberSize: Int, isRevolver: Boolean, isLoaded: Boolean) = {
+        (chamberSize, isRevolver, isLoaded) match {
+            case (_,false,_) => "Ur idiot"
+            case (_,_,false) => "Ur dumbass"
+            case (6,true,true) => if(math.random > 0.5) "U died" else "U won"
+            case _ => "WHAT?"
+        }
+    }
+
+    println(roulette(4,false,false))
+}
+
 
